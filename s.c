13ae1759f753cc597aabb1f4a37d11f85354ca32
yashasvi-goel <31849052+yashasvi-goel@users.noreply.github.com>
2017-12-05 19:04:11 +0530

    Update s.c

--- s.c
+++ s.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+//program to genrate random numbers
+/*
+ * Made by Yashasvi Goel
+ * Enter the string from which words are to be extracted;
+ * the output is enclosed in ':';
+ * Thu Oct 12 09:05:39 IST 2017
+*/
 int main()
 {
 	int *a;
-	a=(int*)malloc(50000*sizeof(int));
+	a=(int*)malloc(50000*sizeof(int));//dynamic allocation
 	for(int i=0;i<50000;i++)
 		a[i]=rand();
 	for(int i=0;i<50000;i++)
